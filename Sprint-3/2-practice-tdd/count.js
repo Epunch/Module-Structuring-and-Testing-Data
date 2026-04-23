48e0a4867c322320089682d50c79c5de1e6977e2
Meysam Razagh <msm.razagh@gmail.com>
2026-04-23 12:10:14 +0200

    feat: complete countChar implementation and tests

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,13 @@
 function countChar(stringOfCharacters, findCharacter) {
-  return 5
+  let variable = 0; // moteghayer baraye shomaresh
+
+  for (let i = 0; i < stringOfCharacters.length; i++) {
+    if (stringOfCharacters[i] === findCharacter) {
+      variable++;
+    }
+  }
+
+  return variable;
 }
 
 module.exports = countChar;
--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -1,5 +1,6 @@
 // implement a function countChar that counts the number of times a character occurs in a string
 const countChar = require("./count");
+
 // Given a string str and a single character char to search for,
 // When the countChar function is called with these inputs,
 // Then it should:
@@ -22,3 +23,10 @@ test("should count multiple occurrences of a character", () => {
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+
+test("should return 0 when no occurrences are found", () => {
+  const str = "hello";
+  const char = "z";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
