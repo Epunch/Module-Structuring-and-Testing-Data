18b1eacb907f6e84a2d4970f3be5da58fe7673ad
Meysam Razagh <msm.razagh@gmail.com>
2026-04-23 12:35:20 +0200

    feat: complete repeatString with TDD

--- Sprint-3/2-practice-tdd/repeat.js
+++ Sprint-3/2-practice-tdd/repeat.js
@@ -1,5 +1,15 @@
-function repeat() {
-  return "hellohellohello";
+function repeat(str, count) {
+  if (count < 0) {
+    throw new Error("Count must be a non-negative integer");
+  }
+
+  let variable = ""; // inja natije ro jam mikonim
+
+  for (let i = 0; i < count; i++) {
+    variable += str;
+  }
+
+  return variable;
 }
 
 module.exports = repeat;
