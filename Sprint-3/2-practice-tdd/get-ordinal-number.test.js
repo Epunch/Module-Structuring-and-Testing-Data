3c7565302d302279a31885d5bcf07c6cee701e67
Meysam Razagh <msm.razagh@gmail.com>
2026-04-23 12:38:43 +0200

    feat: implement getOrdinalNumber with TDD cases

--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -1,13 +1,26 @@
 const getOrdinalNumber = require("./get-ordinal-number");
-// In this week's prep, we started implementing getOrdinalNumber
 
-// continue testing and implementing getOrdinalNumber for additional cases
-// Write your tests using Jest - remember to run your tests often for continual feedback
+// Given a number,
+// When the getOrdinalNumber function is called with this input,
+// Then it should return the correct ordinal string.
 
 // Case 1: Identify the ordinal number for 1
-// When the number is 1,
-// Then the function should return "1st"
-
 test("should return '1st' for 1", () => {
   expect(getOrdinalNumber(1)).toEqual("1st");
+});
+
+// Case 2: Identify the ordinal number for 2
+test("should return '2nd' for 2", () => {
+  expect(getOrdinalNumber(2)).toEqual("2nd");
+});
+
+// Case 3: Identify the ordinal number for 3
+test("should return '3rd' for 3", () => {
+  expect(getOrdinalNumber(3)).toEqual("3rd");
+});
+
+// Case 4: Handle other numbers
+test("should return 'th' for numbers other than 1, 2, or 3", () => {
+  expect(getOrdinalNumber(4)).toEqual("4th");
+  expect(getOrdinalNumber(10)).toEqual("10th");
 });
